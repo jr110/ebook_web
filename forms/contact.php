7c1cee5d6969eb5aff3067b62bf08dc1b3fc1404
jr110 <c.d@hotmail.es>
2022-11-17 10:36:38 -0800

    Iniciasionamiento de conesion a base de datos

--- forms/contact.php
+++ forms/contact.php
@@ -7,21 +7,30 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+ 
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  
+  $Nombre = $_POST['name'];
+  $Email = $_POST['email'];
+  $Tema = $_POST['subject'];
+  $Mensaje =$_POST['message'];
+
+  if(isset($_POST['name']) && !empty($_POST['name']) &&
+    isset($_POST['email']) && !empty($_POST['email']) &&
+    isset($_POST['subject']) && !empty($_POST['subject']) &&
+    isset($_POST['message']) && !empty($_POST['message']))
+    {
+      $EnlaceBD = mysql_connect(localhost,root,"") or die ("No se puedo establecer conexion");
+      mysql_select_db(nombre,$EnlaceBD) or die("No se encontro la base de datos");
+
+      mysql_query("INSERT INTO nombre VALUES('$Nombre','$Email','$Tema','$Mensaje')");
+      echo "Datos enviados correctamente";
+    }
+    else{
+      echo "Error al enviar los datos";
+    }
+
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -33,9 +42,4 @@
   );
   */
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
 ?>
